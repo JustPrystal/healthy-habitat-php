54925fee178f15d39b13334a4e7c1ca5dcd6df2c
Abdul Moiz <abdulmoiz.prystech@gmail.com>
2025-04-17 06:05:10 +0500

    styling fixes

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="../js/script.js"></script>
+    <script src="./js/script.js"></script>
 </head>
 <body>
 
