3c23fd6c9ede0e41dbc5b7a218f5dd5acd6a7363
JustPrystal <ali.usmansajid02@gmail.com>
2025-04-19 06:23:35 +0500

    redirected unauthorized to unauthorized page

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@ session_start();
 
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role'])) {
     // Not logged in
-    header("Location: login.php");
+    header("Location: sign-in.php");
     exit();
 }
 
