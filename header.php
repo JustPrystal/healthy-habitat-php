ab17b992d45b326a12d921830b35e9a3481ac1fe
Anas <anashussain2711@gmail.com>
2025-04-25 23:54:08 +0500

    fav

--- header.php
+++ header.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Healthy Habitat Network</title>
     <link rel="stylesheet" href="./css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="./js/script.js"></script>
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
 </head>
 <body>
 
